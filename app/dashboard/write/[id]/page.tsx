909515706fa8b32da535f274f0b4064a8080ede6
Mercury-nju <2945235656@qq.com>
2025-11-08 08:31:31 +0800

    fix: remove Fragment wrapper and return div directly

--- app/dashboard/write/[id]/page.tsx
+++ app/dashboard/write/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState, Fragment } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter, useParams } from 'next/navigation'
 import { 
   ArrowLeft, 
@@ -402,8 +402,7 @@ export default function WriteEditorPage() {
   }
 
   return (
-    <Fragment>
-      <div className="min-h-screen h-screen bg-white flex" key="main-container">
+    <div className="min-h-screen h-screen bg-white flex">
       {/* Left Sidebar - Simplified */}
       <aside className={(isSidebarOpen ? 'w-16' : 'w-0') + ' bg-white border-r border-gray-200 flex flex-col items-center py-4 transition-all h-full'}>
         {isSidebarOpen && (
@@ -827,7 +826,6 @@ export default function WriteEditorPage() {
         </div>
       </div>
     </div>
-    </Fragment>
   )
 }
 
